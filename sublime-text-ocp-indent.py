92a7712e4d7b96639535b64834b8056f3bec9ace
bluepichu <github@bluepichu.com>
2017-09-24 19:59:35 -0400

    Handle reindent operations transparently

--- sublime-text-ocp-indent.py
+++ sublime-text-ocp-indent.py
@@ -50,30 +50,23 @@ def update_selection_regions(view):
 	view.sel().clear()
 	view.sel().add_all(selection_regions)
 
-class OcpIndentLinesOnInsert(sublime_plugin.TextCommand):
-	def run(self, edit, key):
-		print("indenting on insert")
-
-		# disable sublime's autoindent to prevent double-indenting
-		self.view.settings().set("auto_indent", False)
-
-		# insert the typed character
-		self.view.run_command("insert", { "characters": key })
-
-		# indent lines that are in the selection or are one line before a selection
-		start_end_lines = [(self.view.rowcol(region.a)[0], self.view.rowcol(region.b)[0]) for region in self.view.sel()]
-		lines = {line for (start, end) in start_end_lines for line in range(min(start, end) - 1, max(start, end) + 1)}
-		indent_lines(self.view, edit, lines)
-
-		# update selections - after typing a character, they should be empty
-		update_selection_regions(self.view)
-
 class OcpIndentSelection(sublime_plugin.TextCommand):
-	def run(self, edit):
+	def run(self, edit, include_before = False):
+
 		# indent lines that are in the selection
+		selection_is_empty = True
+
+		for selection in self.view.sel():
+			if selection.a != selection.b:
+				selection_is_empty = False
+				break
+
 		start_end_lines = [(self.view.rowcol(region.a)[0], self.view.rowcol(region.b)[0]) for region in self.view.sel()]
-		lines = {line for (start, end) in start_end_lines for line in range(min(start, end), max(start, end) + 1)}
+		lines = {line for (start, end) in start_end_lines for line in range(min(start, end) + (-1 if include_before else 0), max(start, end) + 1)}
 		indent_lines(self.view, edit, lines)
+
+		if selection_is_empty:
+			update_selection_regions(self.view)
 
 class OcpIndentFile(sublime_plugin.TextCommand):
 	def run(self, edit, indent_empty = True):
@@ -81,20 +74,18 @@ class OcpIndentFile(sublime_plugin.TextCommand):
 		indent_lines(self.view, edit, [line for line in range(0, self.view.rowcol(self.view.size())[0] + 1)], indent_empty = indent_empty)
 
 class OcpIndentEventListener(sublime_plugin.EventListener):
-	lines = 0
-	rewriting = False
-
-	def on_modified(self, view):
-		if is_ocaml(view) and not self.rewriting:
-			self.rewriting = True
-			lines = len(view.substr(sublime.Region(0, view.size())).split("\n")) + 1
-
-			if lines != self.lines:
-				self.lines = lines
-				view.run_command("ocp_indent_file")
-				update_selection_regions(view)
-		self.rewriting = False
+	waiting_for_modify = False
 
 	def on_pre_save(self, view):
 		if is_ocaml(view):
 			view.run_command("ocp_indent_file", { "indent_empty": False })
+
+	def on_modified(self, view):
+		if self.waiting_for_modify:
+			self.waiting_for_modify = False
+			view.run_command("ocp_indent_selection", { "include_before": True })
+
+	def on_query_context(self, view, key, operator, operand, match_all):
+		if key == "ocp_indent_on_insert":
+			self.waiting_for_modify = True
+		return False
