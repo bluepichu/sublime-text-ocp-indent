82ddfb9e31dbe80d3429e6a8115bb755409f81b5
Pinimo <pnmorin@besport.com>
2018-06-15 12:49:18 +0200

    Add options "reindent_on_save" and "opam_switch":
    	- reindent_on_save enables deactivating automatic reindent,
    	  it might be useful when committing on large files
    
    	- opam_switch enables using other opam switches than the
    	  default one (just enter the version, e.g. "4.04.1");
              in particular, on UNIX, this enables using the config file
    	  $HOME/.ocp/.ocp-indent.conf . This was indispensable in my
    	  job, where we make extensive use of lwt (camlp4-like)
    	  syntax.

--- sublime-text-ocp-indent.py
+++ sublime-text-ocp-indent.py
@@ -13,8 +13,12 @@ import subprocess
 
 def plugin_loaded():
 	global reindent_on_tab
+	global reindent_on_save
+	global opam_switch
 	settings = sublime.load_settings("OcpIndent.sublime-settings")
 	reindent_on_tab = settings.get("reindent_on_tab")
+	reindent_on_save = settings.get("reindent_on_save")
+	opam_switch = settings.get("opam_switch")
 
 def is_ocaml(view):
 	return view.match_selector(view.sel()[0].begin(), "source.ocaml")
@@ -22,6 +26,8 @@ def is_ocaml(view):
 def indent_lines(view, edit, lines, indent_empty = True):
 	if not is_ocaml(view):
 		return
+
+	global opam_switch
 
 	command = ["ocp-indent", "--numeric"]
 
@@ -34,7 +40,8 @@ def indent_lines(view, edit, lines, indent_empty = True):
 		stdin = subprocess.PIPE,
 		stdout = subprocess.PIPE,
 		stderr = subprocess.PIPE,
-		universal_newlines = True
+		universal_newlines = True,
+		env = {"PATH":os.getenv("HOME")+"/.opam/"+opam_switch+"/bin/"}
 	)
 
 	content = view.substr(sublime.Region(0, view.size()))
@@ -82,7 +89,8 @@ class OcpIndentEventListener(sublime_plugin.EventListener):
 	waiting_for_modify = False
 
 	def on_pre_save(self, view):
-		if is_ocaml(view):
+		global reindent_on_save
+		if is_ocaml(view) and reindent_on_save:
 			view.run_command("ocp_indent_file", { "indent_empty": False })
 
 	def on_modified(self, view):
